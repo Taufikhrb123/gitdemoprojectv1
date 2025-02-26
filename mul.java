929367540083666f4e18b89e20adc52fd33489c4
Taufiq <mohd.taufik@presto.co.in>
2025-02-26 14:09:44 +0530

    division code

--- mul.java
+++ mul.java
@@ -4,6 +4,8 @@ public class mul {
         int a=10;
 		int b=20;
 		int c=a*b;
+        double d=a/b;
 		System.out.println("This is the mul"+c);
+        System.out.println("this is division"+d);
     }
 }
